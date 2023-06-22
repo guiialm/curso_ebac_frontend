e0ca6af5e0fd2a3205a19688486ecf7630ab26cf
guilherme <guguiejo@gmail.com>
2023-06-22 16:23:16 -0300

    js

--- projeto deposito/java.js
+++ projeto deposito/java.js
@@ -1,1 +1,31 @@
 const form = document.getElementById('form-deposito')
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split (' '); 
+    return nomeComoArray.length >= 2; /* tem que ter dois nomes no nome completo */
+    
+
+}
+
+form.addEventListener('submit', function(e){
+    var formEValido = false;
+
+
+    e.preventDefault(); /* vai fazer com que o submit não apague tudo */ 
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+
+
+    formEValido = (!validaNome(nomeBeneficiario.value)) 
+    if (formEValido) {
+        alert('ERROR 404')
+    } else {
+        alert('tudo ok!')
+    }
+
+
+})
+
+console.log(form);
