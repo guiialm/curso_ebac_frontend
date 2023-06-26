487647189b3ef794edb0b3a863ce4eb4266326c7
guilherme <guguiejo@gmail.com>
2023-06-26 14:36:04 -0300

    finalizado

--- projeto deposito/java.js
+++ projeto deposito/java.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form-deposito')
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split (' '); 
@@ -23,18 +24,31 @@ form.addEventListener('submit', function(e){
 
     formEValido = (!validaNome(nomeBeneficiario.value)) 
     if (formEValido) {
-        alert('ERROR 404')
+        nomeBeneficiario.style.border = '1px solid red'
+        document.querySelector('.error-menssage').style.display = 'block';
     } else {
         const containerMensagemSucesso = document.querySelector('.sucess-mensagem');
         containerMensagemSucesso.innerHTML = mensagemSucesso
         containerMensagemSucesso.style.display = 'block'
 
         nomeBeneficiario.value = '';
-        numeroContaBeneficiario = '';
-        valorDeposito = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
     }
 
 
 })
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup', function(e){
+    console.log(e.target.value);
+    formEValido = (validaNome(e.target.value))
+
+    if (!formEValido) {
+        nomeBeneficiario.classList.add('error')
+        document.querySelector('.error-mensagge').style.display = 'block';
+        
+    } else {
+        nomeBeneficiario.classList.remove('error')
+        document.querySelector('.error-menssage').style.display = 'none';
+    }
+})
