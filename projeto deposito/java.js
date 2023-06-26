d90a1b522d21ac0bd64aff856476f96201ca9ac8
guilherme <guguiejo@gmail.com>
2023-06-26 12:15:28 -0300

    adicionado ediçao aos estilos js

--- projeto deposito/java.js
+++ projeto deposito/java.js
@@ -16,13 +16,22 @@ form.addEventListener('submit', function(e){
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
+    const nomeMaiusculo = nomeBeneficiario.value.toUpperCase();
+    const mensagemSucesso = `Montante de R$: <b>${valorDeposito.value}</b><b>,00</b> depositado para <b>${nomeMaiusculo}</b>; conta : <b>${numeroContaBeneficiario.value}</b>`
+    
 
 
     formEValido = (!validaNome(nomeBeneficiario.value)) 
     if (formEValido) {
         alert('ERROR 404')
     } else {
-        alert('tudo ok!')
+        const containerMensagemSucesso = document.querySelector('.sucess-mensagem');
+        containerMensagemSucesso.innerHTML = mensagemSucesso
+        containerMensagemSucesso.style.display = 'block'
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario = '';
+        valorDeposito = '';
     }
 
 
