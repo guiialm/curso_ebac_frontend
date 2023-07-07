007b9036f3ddc74559087af06a1142e0859c3977
guilherme <guguiejo@gmail.com>
2023-07-07 13:34:05 -0300

    adicionado eventos

--- projeto_media/java.js
+++ projeto_media/java.js
@@ -1,10 +1,32 @@
 const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="images/aprovado.png" alt="emoji festejando" />';
+const imgReprovado = '<img src="images/reprovado.png" alt="emoji triste" />';
+
+
+
+let linhas = '';
+
+
 form.addEventListener('submit', function(e) {
     e.preventDefault(); // e sempre bom por isso.
 
 const inputNomeAtividade = document.getElementById('nome-atividade');
 const inpuNotaAtividade = document.getElementById('nota-atividade');
 
-alert(`Atividade ${inputNomeAtividade.value} - nota: ${inpuNotaAtividade.value}`);
+
+
+let linha = '<tr>';
+linha += `<td> ${inputNomeAtividade.value} </td>`;
+linha += `<td> ${inpuNotaAtividade.value} </td>`;
+linha += `<td> ${inpuNotaAtividade.value >= 7 ? 'imgAprovado' : 'Reprovado'} </td>`;
+linha += '</tr>'
+
+linhas += linha;
+
+const corpoTabela = document.querySelector('tbody');
+corpoTabela.innerHTML = linhas;
+
+inputNomeAtividade.value = '';
+inpuNotaAtividade.value = '';
 
 });   
